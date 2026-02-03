2b1feb6f04967def457528f9d965ab2b1756ed8f
Kliment Mamykin <kliment@amperon.co>
2026-02-02 23:51:27 -0500

    Removed cloud-run and terraform

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-# Terraform state backend configuration
-# The bucket will be created by scripts/setup.sh
-
-terraform {
-  backend "gcs" {
-    bucket = "openclaw-kmamyk-terraform-state"
-    prefix = "openclaw/compute"
-  }
-}
